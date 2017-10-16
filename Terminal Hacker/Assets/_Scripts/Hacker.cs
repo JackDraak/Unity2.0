be1677d3207206138a18ad5fd4925ff6064dfac8
Jack Draak <jackdraak@gmail.com>
2017-10-16 01:46:03 -0400

    Basic functionality working (begin Sec2 Lec21 Unity2.0)

--- Terminal Hacker/Assets/_Scripts/Hacker.cs
+++ Terminal Hacker/Assets/_Scripts/Hacker.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Hacker : MonoBehaviour {
     // Data
+    const int wordsPerLevel = 5;
     string[] wordsOne = { "blue", "pink", "green", "yellow", "purple", "orange" };
     string[] wordsTwo = { "duke", "coco", "tiger", "buddy", "bandit", "sunny" };
     string[] wordsThree = { "wells", "clarke", "gibson", "asimov", "bradbury", "heinlein" };
@@ -14,11 +15,11 @@ public class Hacker : MonoBehaviour {
     int wordTwoPos = 0;
     int wordThreePos = 0;
     int wordFourPos = 0;
-    int tokens = 10;                        // Game currency
-    int currentValue = 0;                   // Effective difficulty level & TOA factor [depreciated?]
-    enum Screen { Menu, Help, Guess, Pass}  // Game state 
-    Screen currentScreen;                   // Game state placeholder
-    string scrambleWord;                    // Placeholder for current scramble-word
+    int tokens = 10;                                // Game currency
+    int currentValue = 0;                           // Effective difficulty level & TOA factor [depreciated?]
+    enum Screen { Menu, Help, Guess, Pass, Fail}    // Game state 
+    Screen currentScreen;                           // Game state placeholder
+    string scrambleWord;                            // Placeholder for current scramble-word
 
 	// Use this for initialization
 	void Start ()
@@ -34,17 +35,20 @@ public class Hacker : MonoBehaviour {
 
     void OnUserInput(string input)
     {
-        if (input == "menu") ShowMenu();
+        if (tokens <= 0) ShowFail();
+        else if (input == "menu") ShowMenu();
         else if (input == "?") ShowHelp();
         else if (currentScreen == Screen.Menu) HandleMenuInput(input);
         else if (currentScreen == Screen.Guess) HandleGuessInput(input);
         else if (currentScreen == Screen.Pass) HandlePassInput(input);
         else if (currentScreen == Screen.Help) HandleHelpInput(input);
+        else if (currentScreen == Screen.Fail) HandleFailInput(input);
     }
 
-    private void HandleMenuInput(string input)
+     void HandleMenuInput(string input)
     {
-        if (input == "1") Level(1);
+        if (currentScreen == Screen.Fail) return;
+        else if (input == "1") Level(1);
         else if (input == "2") Level(2);
         else if (input == "3") Level(3);
         else if (input == "4") Level(4);
@@ -56,20 +60,70 @@ public class Hacker : MonoBehaviour {
 
     }
 
-    private void HandleGuessInput(string input)
+    void HandleGuessInput(string input)
     {
         Terminal.WriteLine("Guess Input: " + input);
+        if (input == scrambleWord)
+        {
+            tokens += (currentValue * 2);
+            if (currentValue == 1)
+            {
+                if (wordOnePos < wordsPerLevel) wordOnePos++;
+                else wordOnePos = 0;
+            }
+            if (currentValue == 2)
+            {
+                if (wordTwoPos < wordsPerLevel) wordTwoPos++;
+                else wordTwoPos = 0;
+            }
+            if (currentValue == 3)
+            {
+                if (wordThreePos < wordsPerLevel) wordThreePos++;
+                else wordThreePos = 0;
+            }
+            if (currentValue == 4)
+            {
+                if (wordFourPos < wordsPerLevel) wordFourPos++;
+                else wordFourPos = 0;
+            }
+            currentScreen = Screen.Pass; // Really? Maybe save this for the final ending?
+            Terminal.WriteLine("Congratulations! You've earned " + (currentValue * 2) + " TOA.");
+            Terminal.WriteLine("[TOA: " + tokens +"]");
+        }
+        else
+        {
+            tokens -= currentValue;
+            Terminal.WriteLine("Bummer! You've lost " + currentValue + " TOA.");
+            Terminal.WriteLine("[TOA: " + tokens + "]");
+        }
     }
 
-    private void HandlePassInput(string input)
+    void HandlePassInput(string input)
     {
         Terminal.WriteLine("Pass Input: " + input);
+        Terminal.WriteLine("\nPlease enter 'menu' at any time, or '?' for help.\n" +
+                           "otherwise use the menu then make a selection [#].\n\n" +
+                           "[TOA: " + tokens + "]");
     }
 
-    private void HandleHelpInput(string input)
+    void HandleHelpInput(string input)
     {
         ShowHelp();
         Terminal.WriteLine("Syntax Error: " + input);
+    }
+
+    void HandleFailInput(string input)
+    {
+        ShowFail();
+        Terminal.WriteLine("Syntax Error: " + input);
+    }
+
+    void ShowFail()
+    {
+        Terminal.WriteLine("\nIt seems you have run out of TOA. If I were in\n" +
+                   "your shoes, I'd be making myself well, scarce...\n" +
+                   "now!\n\n" +
+                   "[TOA: " + tokens + "]");
     }
 
     void ShowHelp()
@@ -98,20 +152,25 @@ public class Hacker : MonoBehaviour {
         currentScreen = Screen.Guess;
         Terminal.ClearScreen();
         Terminal.WriteLine("GTHDB Level " + currentValue + " | TOA: " + tokens);
+        Terminal.WriteLine("This group is worth " + currentValue + " TOA for each answer.");
+        Terminal.WriteLine("Unscramble the answer to the security question:");
+        Terminal.WriteLine("");
         if (currentValue == 1) Terminal.WriteLine("What is your favourite colour?");
         else if (currentValue == 2) Terminal.WriteLine("What is the name of your first pet?");
         else if (currentValue == 3) Terminal.WriteLine("Who is your favourite SciFi author?");
         else if (currentValue == 4) Terminal.WriteLine("What is the name of the street you grew up on?");
-        Terminal.WriteLine("");
-        Terminal.WriteLine("This group is worth " + currentValue + " TOA for each answer.");
-        Terminal.WriteLine("Unscramble the answer to the security question:");
         Terminal.WriteLine("");
         ShowScramble(level);
     }
 
     void ShowScramble(int level)
     {
-        Terminal.WriteLine("Scramble Level " + level + ": __TBD__");
+        if (level == 1) scrambleWord = wordsOne[wordOnePos];
+        else if (level == 2) scrambleWord = wordsTwo[wordTwoPos];
+        else if (level == 3) scrambleWord = wordsThree[wordThreePos];
+        else if (level == 4) scrambleWord = wordsFour[wordFourPos];
+        string currentWord = scrambleWord.Anagram();
+        Terminal.WriteLine("Scramble Level " + level + ": " + currentWord);
     }
 
     void ShowMenu()
