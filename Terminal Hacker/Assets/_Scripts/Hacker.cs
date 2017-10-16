c4dedccd84ed44843b6dfef7b1f25c9eb1487440
Jack Draak <jackdraak@gmail.com>
2017-10-16 00:41:22 -0400

    mid-refactor, Section 2 lecture 21

--- Terminal Hacker/Assets/_Scripts/Hacker.cs
+++ Terminal Hacker/Assets/_Scripts/Hacker.cs
@@ -3,16 +3,27 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Hacker : MonoBehaviour {
+    // Data
+    string[] wordsOne = { "blue", "pink", "green", "yellow", "purple", "orange" };
+    string[] wordsTwo = { "duke", "coco", "tiger", "buddy", "bandit", "sunny" };
+    string[] wordsThree = { "wells", "clarke", "gibson", "asimov", "bradbury", "heinlein" };
+    string[] wordsFour = { "maypole", "wellwood", "spadina", "townsend", "nostrand", "divisadero" };
+
+    // Initial settings
+    int wordOnePos = 0;
+    int wordTwoPos = 0;
+    int wordThreePos = 0;
+    int wordFourPos = 0;
     int tokens = 10;                        // Game currency
-    int level = 0;                          // Game state -- depreciated?
-    int currentValue = 0;                   // Effective difficulty level
+    int currentValue = 0;                   // Effective difficulty level & TOA factor [depreciated?]
     enum Screen { Menu, Help, Guess, Pass}  // Game state 
-    Screen currentScreen;
+    Screen currentScreen;                   // Game state placeholder
+    string scrambleWord;                    // Placeholder for current scramble-word
 
 	// Use this for initialization
 	void Start ()
     {
-        ShowMenu();
+        ShowMenu(); // OnUserInput is the primary controller, initialize scene with ShowMenu()
     }
 	
 	// Update is called once per frame
@@ -24,99 +35,101 @@ public class Hacker : MonoBehaviour {
     void OnUserInput(string input)
     {
         if (input == "menu") ShowMenu();
+        else if (input == "?") ShowHelp();
         else if (currentScreen == Screen.Menu) HandleMenuInput(input);
+        else if (currentScreen == Screen.Guess) HandleGuessInput(input);
+        else if (currentScreen == Screen.Pass) HandlePassInput(input);
+        else if (currentScreen == Screen.Help) HandleHelpInput(input);
     }
 
     private void HandleMenuInput(string input)
     {
-        if (input == "1") LevelOne();
-        else if (input == "2") LevelTwo();
+        if (input == "1") Level(1);
+        else if (input == "2") Level(2);
+        else if (input == "3") Level(3);
+        else if (input == "4") Level(4);
         else
         {
- /*           if (level == -1)
-            {
-                ShowHelp();
-                Terminal.WriteLine("Syntax Error: " + input);
-            } */
-            if (level == 0)
-            {
-                ShowMenu();
-                Terminal.WriteLine("Syntax Error: " + input);
-            }
-            else if (level == 1)
-            {
-                LevelOne();
-                Terminal.WriteLine("Syntax Error: " + input);
-            }
-            else if (level == 2)
-            {
-                LevelTwo();
-                Terminal.WriteLine("Syntax Error: " + input);
-            }
+            ShowMenu();
+            Terminal.WriteLine("Syntax Error: " + input);
         }
+
     }
 
-/*    void ShowHelp()
+    private void HandleGuessInput(string input)
     {
-        level = -1;
+        Terminal.WriteLine("Guess Input: " + input);
+    }
+
+    private void HandlePassInput(string input)
+    {
+        Terminal.WriteLine("Pass Input: " + input);
+    }
+
+    private void HandleHelpInput(string input)
+    {
+        ShowHelp();
+        Terminal.WriteLine("Syntax Error: " + input);
+    }
+
+    void ShowHelp()
+    {
         currentScreen = Screen.Help;
         Terminal.ClearScreen();
         Terminal.WriteLine("New user assistance:");
-        Terminal.WriteLine("");
         Terminal.WriteLine("This terminal can be controlled by entering --");
         Terminal.WriteLine("");
         Terminal.WriteLine("   ? - will display the user help.");
         Terminal.WriteLine("   menu - will display the Main Menu.");
         Terminal.WriteLine("   [#] - select menu options for further options.");
         Terminal.WriteLine("");
+        Terminal.WriteLine(" * While descrambling security question answers it");
+        Terminal.WriteLine("   costs specific TOA to make a guess, but if you");
+        Terminal.WriteLine("   are correct you will double your TOA. If you");
+        Terminal.WriteLine("   deplete your cache of TOA, you will be erased.");
+        Terminal.WriteLine("\nPlease enter 'menu' at any time, or '?' for help.\n" +
+                           "otherwise use the menu then make a selection [#].\n\n" +
+                           "[TOA: " + tokens + "]");
     }
-*/
 
-    void LevelOne()
-    {
-        level = 1;
+    void Level(int level)
+        {
         currentValue = level;
         currentScreen = Screen.Guess;
         Terminal.ClearScreen();
-        Terminal.WriteLine("GTHDB Level One:\n" +
-                           "What is your favourite colour?");
+        Terminal.WriteLine("GTHDB Level " + currentValue + " | TOA: " + tokens);
+        if (currentValue == 1) Terminal.WriteLine("What is your favourite colour?");
+        else if (currentValue == 2) Terminal.WriteLine("What is the name of your first pet?");
+        else if (currentValue == 3) Terminal.WriteLine("Who is your favourite SciFi author?");
+        else if (currentValue == 4) Terminal.WriteLine("What is the name of the street you grew up on?");
         Terminal.WriteLine("");
-        Terminal.WriteLine("(This group is worth " + currentValue + " TOA for each answer)");
+        Terminal.WriteLine("This group is worth " + currentValue + " TOA for each answer.");
+        Terminal.WriteLine("Unscramble the answer to the security question:");
         Terminal.WriteLine("");
+        ShowScramble(level);
     }
 
-    void LevelTwo()
+    void ShowScramble(int level)
     {
-        level = 2;
-        currentValue = level;
-        currentScreen = Screen.Guess;
-        Terminal.ClearScreen();
-        Terminal.WriteLine("GTHDB Level Two:\n" +
-                           "What is the name of your first pet?");
-        Terminal.WriteLine("");
-        Terminal.WriteLine("(This group is worth " + currentValue + " TOA for each answer)");
-        Terminal.WriteLine("");
+        Terminal.WriteLine("Scramble Level " + level + ": __TBD__");
     }
 
     void ShowMenu()
     {
-        level = 0;
         currentScreen = Screen.Menu;
         Terminal.ClearScreen();
-        //  ........10........20........30........40........50........60........70........80
-        //  Terminal.WriteLine("0123456789012345678901234567890123456789---3456789012345678901234567890123456789");
-        // Terminal.WriteLine("Welcome, " + user + ", to the GTHDB.\n");
         Terminal.WriteLine("Thank you for your assistance with our global\n" +
                            "distributed effort which we call 'Terminal-Hacker'\n" +
                            "You will be rewarded tokens of appreciation (TOA)\n" +
                            "for your success. Please work dilligently however,\n" +
                            "as failures will not be accomodated.\n");
-        Terminal.WriteLine("Please descramble one of the following:\n\n" +
+        Terminal.WriteLine("Please descramble one of the following:\n" +
                            "  1) What is your favourite colour?\n" +
-                           "  2) What is the name of your first pet?\n"); // +
-                   //        "  3) Who is your favourite SciFi author?\n" +
-                   //        "  4) Name of the street you grew up on?");
-        Terminal.WriteLine("\n" +
-                           "[TOA: " + tokens + "]"); // TODO replace with actual TOA count
+                           "  2) What is the name of your first pet?\n" +
+                           "  3) Who is your favourite SciFi author?\n" +
+                           "  4) Name of the street you grew up on?");
+        Terminal.WriteLine("\nPlease enter 'menu' at any time, or '?' for help.\n" +
+                           "otherwise, please make a menu selection [#].\n\n" +
+                           "[TOA: " + tokens + "]");
     }
 }
