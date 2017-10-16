35a433a051df5bcc6d5e200e5c409603487a524d
Jack Draak <jackdraak@gmail.com>
2017-10-16 11:58:47 -0400

    tidy

--- Terminal Hacker/Assets/_Scripts/Hacker.cs
+++ Terminal Hacker/Assets/_Scripts/Hacker.cs
@@ -4,11 +4,11 @@ using UnityEngine;
 
 public class Hacker : MonoBehaviour {
     // Data
-    const int wordsPerLevel = 5;
     string[] wordsOne = { "blue", "pink", "green", "yellow", "purple", "orange" };
     string[] wordsTwo = { "duke", "coco", "tiger", "buddy", "bandit", "sunny" };
     string[] wordsThree = { "wells", "clarke", "gibson", "asimov", "bradbury", "heinlein" };
     string[] wordsFour = { "maypole", "wellwood", "spadina", "townsend", "nostrand", "divisadero" };
+    int wordsPerLevel = 5; // NOTE: Count starts at zero, therefore 5 wPL = 6 words per level.
 
     // Initial settings
     int wordOnePos = 0;
@@ -16,22 +16,15 @@ public class Hacker : MonoBehaviour {
     int wordThreePos = 0;
     int wordFourPos = 0;
     int tokens = 10;                                // Game currency
-    int currentValue = 0;                           // Effective difficulty level & TOA factor [depreciated?]
-    enum Screen { Menu, Help, Guess, Pass, Fail}    // Game state 
+    int currentValue;                               // Effective difficulty level & TOA factor
+    enum Screen { Menu, Help, Guess, Pass, Fail}    // Game state enum
     Screen currentScreen;                           // Game state placeholder
     string scrambleWord;                            // Placeholder for current scramble-word
 
-	// Use this for initialization
 	void Start ()
     {
         ShowMenu(); // OnUserInput is the primary controller, initialize scene with ShowMenu()
     }
-	
-	// Update is called once per frame
-	void Update ()
-    {
-		
-	}
 
     void OnUserInput(string input)
     {
@@ -45,7 +38,7 @@ public class Hacker : MonoBehaviour {
         else if (currentScreen == Screen.Fail) HandleFailInput(input);
     }
 
-     void HandleMenuInput(string input)
+    void HandleMenuInput(string input)
     {
         if (currentScreen == Screen.Fail) return;
         else if (input == "1") Level(1);
@@ -118,12 +111,23 @@ public class Hacker : MonoBehaviour {
         Terminal.WriteLine("Syntax Error: " + input);
     }
 
-    void ShowFail()
+    void ShowMenu()
     {
-        Terminal.WriteLine("\nIt seems you have run out of TOA. If I were in\n" +
-                   "your shoes, I'd be making myself well, scarce...\n" +
-                   "now!\n\n" +
-                   "[TOA: " + tokens + "]");
+        currentScreen = Screen.Menu;
+        Terminal.ClearScreen();
+        Terminal.WriteLine("Thank you for your assistance with our global\n" +
+                           "distributed effort which we call 'Terminal-Hacker'\n" +
+                           "You will be rewarded tokens of appreciation (TOA)\n" +
+                           "for your success. Please work dilligently however,\n" +
+                           "as failures will not be accomodated.\n");
+        Terminal.WriteLine("Please descramble one of the following:\n" +
+                           "  1) What is your favourite colour?\n" +
+                           "  2) What is the name of your first pet?\n" +
+                           "  3) Who is your favourite SciFi author?\n" +
+                           "  4) Name of the street you grew up on?");
+        Terminal.WriteLine("\nPlease enter 'menu' at any time, or '?' for help.\n" +
+                           "otherwise, please make a menu selection [#].\n\n" +
+                           "[TOA: " + tokens + "]");
     }
 
     void ShowHelp()
@@ -146,6 +150,24 @@ public class Hacker : MonoBehaviour {
                            "[TOA: " + tokens + "]");
     }
 
+    void ShowFail()
+    {
+        Terminal.WriteLine("\nIt seems you have run out of TOA. If I were in\n" +
+                   "your shoes, I'd be making myself well, scarce...\n" +
+                   "now!\n\n" +
+                   "[TOA: " + tokens + "]");
+    }
+
+    void ShowScramble(int level)
+    {
+        if (level == 1) scrambleWord = wordsOne[wordOnePos];
+        else if (level == 2) scrambleWord = wordsTwo[wordTwoPos];
+        else if (level == 3) scrambleWord = wordsThree[wordThreePos];
+        else if (level == 4) scrambleWord = wordsFour[wordFourPos];
+        string currentWord = scrambleWord.Anagram();
+        Terminal.WriteLine("Scramble Level " + level + ": " + currentWord);
+    }
+
     void Level(int level)
         {
         currentValue = level;
@@ -159,36 +181,7 @@ public class Hacker : MonoBehaviour {
         else if (currentValue == 2) Terminal.WriteLine("What is the name of your first pet?");
         else if (currentValue == 3) Terminal.WriteLine("Who is your favourite SciFi author?");
         else if (currentValue == 4) Terminal.WriteLine("What is the name of the street you grew up on?");
-        Terminal.WriteLine("");
         ShowScramble(level);
     }
 
-    void ShowScramble(int level)
-    {
-        if (level == 1) scrambleWord = wordsOne[wordOnePos];
-        else if (level == 2) scrambleWord = wordsTwo[wordTwoPos];
-        else if (level == 3) scrambleWord = wordsThree[wordThreePos];
-        else if (level == 4) scrambleWord = wordsFour[wordFourPos];
-        string currentWord = scrambleWord.Anagram();
-        Terminal.WriteLine("Scramble Level " + level + ": " + currentWord);
-    }
-
-    void ShowMenu()
-    {
-        currentScreen = Screen.Menu;
-        Terminal.ClearScreen();
-        Terminal.WriteLine("Thank you for your assistance with our global\n" +
-                           "distributed effort which we call 'Terminal-Hacker'\n" +
-                           "You will be rewarded tokens of appreciation (TOA)\n" +
-                           "for your success. Please work dilligently however,\n" +
-                           "as failures will not be accomodated.\n");
-        Terminal.WriteLine("Please descramble one of the following:\n" +
-                           "  1) What is your favourite colour?\n" +
-                           "  2) What is the name of your first pet?\n" +
-                           "  3) Who is your favourite SciFi author?\n" +
-                           "  4) Name of the street you grew up on?");
-        Terminal.WriteLine("\nPlease enter 'menu' at any time, or '?' for help.\n" +
-                           "otherwise, please make a menu selection [#].\n\n" +
-                           "[TOA: " + tokens + "]");
-    }
 }
