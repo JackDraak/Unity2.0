9bbed914fd60d3d31da31479acb6f5e8eaf1219c
Jack Draak <jackdraak@gmail.com>
2017-10-20 09:55:12 -0400

    wip

---
+++ Drone Patrol/Assets/_Scripts/CameraController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+    [SerializeField] GameObject player;
+    [SerializeField] float margin = 10; 
+    private float offset;
+    private Vector3 newPos;
+
+    // Use this for initialization
+    void Start () {
+        offset = transform.position.x - player.transform.position.x;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    // LateUpdate is called after eache Update frame
+    void LateUpdate()
+    {
+        // newPos = new Vector3 (player.transform.position.x + (offset - margin), 
+        //    transform.position.y, transform.position.z);
+
+        // transform.position = newPos;
+
+      
+        Vector3 velocity = Vector3.zero;
+        Vector3 forward = player.transform.forward * 10.0f;
+        Vector3 needPos = player.transform.position - forward;
+        transform.position = Vector3.SmoothDamp(transform.position, needPos,
+                                                ref velocity, 0.05f);
+        transform.LookAt(player.transform);
+        // transform.rotation = player.transform.rotation;
+
+    }
+}
--- Drone Patrol/Assets/_Scripts/DroneController.cs
+++ Drone Patrol/Assets/_Scripts/DroneController.cs
@@ -4,40 +4,105 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class DroneController : MonoBehaviour {
+    [SerializeField] int rcsThrust = 275;
+    [SerializeField] int mainThrust = 850;
+    [SerializeField] ParticleSystem particleSystem;
+    Rigidbody rigidbody;
+    AudioSource audioSource;
+    Vector3 startPosition;
+    Quaternion startRotation;
+    bool thrustAudio;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+    // Use this for initialization.
+    void Start () {
+        startPosition = transform.position;
+        startRotation = transform.rotation;
+        rigidbody = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
+    }
 	
-	// Update is called once per frame
+	// Update is called once per frame.
 	void Update () {
         ProcessInput();
+        ProcessVisualEffects();
+        ProcessAudio();
 	}
 
     private void ProcessInput()
     {
-        if (Input.GetKey(KeyCode.W)) Thrust(0); // Main thrust
-        if (Input.GetKey(KeyCode.A)) { Thrust(-1); return; } // Port thrust
-        else if (Input.GetKey(KeyCode.D)) { Thrust(1); return; } // Starboard thrust
-        return;
+    // Data
+        // TODO: allow swap port/starboard numbers if player wants inverted controls.
+        int port = -1;
+        int starboard = 1;
+        const int main = 0;
+
+    // Inputs we care about
+        bool key_w = Input.GetKey(KeyCode.W);
+        bool key_a = Input.GetKey(KeyCode.A);
+        bool key_d = Input.GetKey(KeyCode.D);
+        bool key_ua = Input.GetKey(KeyCode.UpArrow);
+        bool key_la = Input.GetKey(KeyCode.LeftArrow);
+        bool key_ra = Input.GetKey(KeyCode.RightArrow);
+        bool key_q = Input.GetKey(KeyCode.Q);
+        bool key_r = Input.GetKey(KeyCode.R);
+        bool key_lc = Input.GetKey(KeyCode.LeftControl);
+
+    // Input parsing...
+        // ...Special-case cues
+        Quit(key_q);
+        ResetPlayer(key_r);
+
+        // ...Audio cues
+        thrustAudio = (key_w || key_a || key_d || key_ua || key_la || key_ra);
+
+        // ...Thrust cues
+        if (key_w || key_ua) ActivateThrust(main);
+        if (key_a || key_la) { ActivateThrust(port); return; }
+        else if (key_d || key_ra) { ActivateThrust(starboard); return; }
     }
 
-    private void Thrust(int v)
+    private static void Quit(bool key_q)
     {
-        switch(v)
+        // TODO: make this Ctrl-Q or Alt-X?
+        if (key_q) Application.Quit(); 
+    }
+
+    private void ResetPlayer(bool key_r)
+    {
+        // TODO: make this Ctrl-R?
+        if (key_r)
         {
-            case -1: // Port
-                Debug.Log("Port");
-                break;
-            case 0: // Main
-                Debug.Log("Main");
-                break;
-            case 1: // Starboard
-                Debug.Log("Starboard");
-                break;
-            default:
-                throw new NotImplementedException();
+            rigidbody.isKinematic = true; // Nullify velocity
+            transform.position = startPosition;
+            transform.rotation = startRotation;
+            rigidbody.isKinematic = false; // Re-enable physics
+        }
+    }
+
+    private void ProcessAudio()
+    {
+        var audioOn = audioSource.isPlaying;
+        if (!audioOn && thrustAudio) audioSource.Play();
+        else if (audioOn && !thrustAudio) audioSource.Stop();
+    }
+
+    private void ProcessVisualEffects()
+    {
+        if (thrustAudio && particleSystem.isStopped) particleSystem.Play();
+        else if (particleSystem.isPlaying && !thrustAudio) particleSystem.Stop();
+    }
+
+    private void ActivateThrust(int v)
+    {
+        float rotationForce = rcsThrust * Time.deltaTime;
+        float thrustForce = mainThrust * Time.deltaTime;
+
+        if (v == 0) rigidbody.AddRelativeForce(Vector3.up * thrustForce);
+        else
+        {
+            rigidbody.freezeRotation = true;
+            transform.Rotate((-v) * Vector3.forward * rotationForce);
+            rigidbody.freezeRotation = false;
         }
     }
 }
