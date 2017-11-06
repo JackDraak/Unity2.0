5dc5f71f14691e3b9a6bedb1d52e128ec67a2f6e
Jack Draak <jackdraak@gmail.com>
2017-11-06 14:49:55 -0500

    begin Quasar Dawg

---
+++ Quasar Dawg/Assets/LevelManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour {
+    static LevelManager instance = null;
+
+    void Start()
+    {
+        if (instance != null && instance != this) { Destroy(gameObject); }
+        else { instance = this; GameObject.DontDestroyOnLoad(gameObject); }
+    }
+
+    private void OnEnable()
+    {
+        Invoke("LoadNextLevel", 5f);
+    }
+
+    void LoadNextLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
