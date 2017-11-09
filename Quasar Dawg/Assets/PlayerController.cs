5a20ef656400a645b00d1b2701a00552b00db01e
Jack Draak <jackdraak@gmail.com>
2017-11-09 02:45:36 -0500

    swap playercontroller

--- Quasar Dawg/Assets/PlayerController.cs
+++ Quasar Dawg/Assets/PlayerController.cs
@@ -1,31 +1,79 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 using UnityStandardAssets.CrossPlatformInput;
 
 public class PlayerController : MonoBehaviour {
-    [Range(1, 100000)][SerializeField] float sFactor = 20000f;
+    [Header("Values to tweak right now:")]
+    [Range(-10f, 10f)][Tooltip("factor for rotation skew")][SerializeField] float rFactorX = 0f;
+    [Range(-10f, 10f)] [Tooltip("factor for rotation skew")] [SerializeField] float rFactorY = 0f;
+    [Space(10)]
+    [Header("Generally, leave these alone, they've been dialed-in:")]
+    [Tooltip("Range of motion, in m")] [SerializeField] float xRange = 3.7f;
+    [Tooltip("Range of motion, in m")][SerializeField] float yRange = 1.9f;
+    [Tooltip("Speed, in ms^-1")][SerializeField] float speed = 4f;
+
+    private Rigidbody rigidbody;
     private bool alive = true;
+    private bool shoot = false;
+    private bool pause = false;
     private float delta;
     private float inputX; 
     private float inputY;
-    private Rigidbody rigidbody;
 
-    void Start()        { rigidbody = GetComponent<Rigidbody>(); delta = Time.fixedDeltaTime; }
-    void Update()       { if (alive) PollControls(); }
-    void FixedUpdate()  { UpdatePlayerPosition(); }
+    void Start()
+    {
+        if (!(rigidbody = GetComponent<Rigidbody>()))
+            Debug.Log(this + " Start() failed to attached Player rigidbody!");
+    }
+
+    void FixedUpdate()
+    {
+        UpdatePlayerPosition();
+    }
 
     void UpdatePlayerPosition()
     {
-        // TODO: ship needs to be glued to camera by distance. Bonus points, tilt around corners!
-        if ((inputY == 0 && inputX == 0) || !alive) return;
-        if (inputX != 0) 
-            rigidbody.AddRelativeForce(Vector3.right * inputX * delta * sFactor);
-        if (inputY != 0) 
-            rigidbody.AddRelativeForce(Vector3.up * inputY * delta * sFactor);
+        if (!alive) return;
+        delta = Time.deltaTime;
+        PollControls();
+        SetLocalPosition();
+        // SetLocalAngles();
+    }
+
+    private void SetLocalPosition()
+    {
+        if ((float.IsNaN(inputX) && float.IsNaN(inputY)) || !alive) return;
+        // special y-offset gives ship more space at the top of the frame for "balance".
+        float overhead = 0.5f;
+
+        float xOffset = inputX * speed * delta;
+        float yOffset = inputY * speed * delta;
+        float desiredXpos = transform.localPosition.x + xOffset;
+        float desiredYpos = transform.localPosition.y + yOffset;
+        float clampedXPos = Mathf.Clamp(desiredXpos, -xRange, xRange);
+        float clampedYPos = Mathf.Clamp(desiredYpos, -yRange, yRange + overhead);
+
+        transform.localPosition = new Vector3(clampedXPos, clampedYPos, transform.localPosition.z);
+    }
+
+    private void SetLocalAngles()
+    {
+        if ((float.IsNaN(rFactorX) && float.IsNaN(rFactorY)) || !alive) return;
+        // X Y Z = Pitch Yaw Roll, respectively.
+        float xPos = transform.localPosition.x;
+        float yPos = transform.localPosition.y;
+        float roll = 0; // transform.localRotation.eulerAngles.z;
+        float pitch = xPos * rFactorX * delta;
+        float yaw = yPos * rFactorY * delta;
+
+        transform.Rotate(new Vector3(pitch, yaw, roll));
     }
 
     private void PollControls()
     {
-        inputX = CrossPlatformInputManager.GetAxis("Horizontal"); 
+        if (!alive) return;
+        inputX = CrossPlatformInputManager.GetAxis("Horizontal");
         inputY = CrossPlatformInputManager.GetAxis("Vertical");
+        shoot = CrossPlatformInputManager.GetButton("Fire1");
     }
 }
