3309f280dcb4a325757f31a5925478cdc85180a9
Jack Draak <jackdraak@gmail.com>
2017-12-10 18:39:19 -0500

    feature complete

--- Analog Clock/Assets/AnalogClock.cs
+++ Analog Clock/Assets/AnalogClock.cs
@@ -13,17 +13,17 @@ public class AnalogClock : MonoBehaviour
     [SerializeField] Transform stopwatchHand;
 
     private AudioSource audioSource;
-    private bool mute = false, stopwatch = false;
-    private float intervalGUIUpdate, updateGUITime;
+    private bool mute = false, stopwatch = false, view = true;
+    private float intervalGUIUpdate, updateElapsedTimer;
     private float hourMinuteRotation;
     private float minuteRotation = 0.1f;
     private float twelveHourRotation = (0.1f / 12);
     private float updateInterval = 1;
-    private float updateTime;
+    private float updateTimer;
     private int hourRotation = 30;
     private int secondRotation = 6;
     private DateTime currentTime, startTime, stopTime;
-    private TimeSpan breakTime;
+    private TimeSpan breakTime, runTime;
     private Vector3 clockwise = new Vector3(0, 1, 0);
 
     private void Start()
@@ -32,16 +32,17 @@ public class AnalogClock : MonoBehaviour
         hourMinuteRotation = hourRotation * twelveHourRotation * 2;
         intervalGUIUpdate = updateInterval / 10;
         startTime = DateTime.Now;
-        UpdateClock();
         topRight.text = ("0ms :Stopwatch\n(space)");
+        UpdateClock();
     }
 
     private void Update()
     {
         ControlMute();
         ControlStopwatch();
-        if (Time.time >= updateTime) UpdateClock();
-        if (Time.time >= updateGUITime) UpdateGUI();
+        ControlVisability();
+        if (Time.time >= updateTimer) UpdateClock();
+        if (Time.time >= updateElapsedTimer) UpdateElapsedTime();
     }
 
     private void ControlMute()
@@ -73,9 +74,33 @@ public class AnalogClock : MonoBehaviour
         }
     }
 
+    private void ControlVisability()
+    {
+        if (Input.GetKeyDown(KeyCode.V))
+        {
+            Color invisible = new Color(0, 0, 0, 0);
+            Color visible = new Color(250, 250, 250, 250);
+            view = !view;
+            if (!view)
+            {
+                topLeft.color = invisible;
+                topRight.color = invisible;
+                bottomLeft.color = invisible;
+                bottomRight.color = invisible;
+            }
+            else
+            {
+                topLeft.color = visible;
+                topRight.color = visible;
+                bottomLeft.color = visible;
+                bottomRight.color = visible;
+            }
+        }
+    }
+
     private void UpdateClock()
     {
-        updateTime += updateInterval;
+        updateTimer += updateInterval;
 
         if (!mute) audioSource.PlayOneShot(secondHandFX[Mathf.FloorToInt(URandom.Range(0, secondHandFX.Length))]);
 
@@ -87,10 +112,9 @@ public class AnalogClock : MonoBehaviour
         hourHand.rotation = Quaternion.Euler(0f, (currentTime.Hour * hourRotation) + (currentTime.Minute * hourMinuteRotation), 0f);
     }
 
-    private void UpdateGUI()
+    private void UpdateElapsedTime()
     {
-        updateGUITime += intervalGUIUpdate;
-
-        topLeft.text = ("Begun: " + startTime.ToString() + "\nElapsed: " + Time.time.ToString());
+        runTime = DateTime.Now - startTime;
+        topLeft.text = ("Begun: " + startTime.ToString() + "\nElapsed: " + runTime.ToString() + "\nV = toggle overlay");
     }
 }
