512baacd9b075faf3043d9c7e3bc6da41387563c
Jack Draak <jackdraak@gmail.com>
2017-12-10 14:24:44 -0500

    some UI improvements, including making stopwatch hand move smoothly.

--- Analog Clock/Assets/AnalogClock.cs
+++ Analog Clock/Assets/AnalogClock.cs
@@ -1,14 +1,16 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
+using System;
+using URandom = UnityEngine.Random;
 
 public class AnalogClock : MonoBehaviour 
 {
+    [SerializeField] AudioClip[] secondHandFX;
+    [SerializeField] Text bottomLeft, bottomRight, topLeft, topRight;
     [SerializeField] Transform hourHand;
     [SerializeField] Transform minuteHand;
     [SerializeField] Transform secondHand;
     [SerializeField] Transform stopwatchHand;
-    [SerializeField] Text bottomLeft, topLeft, topRight;
-    [SerializeField] AudioClip[] secondHandFX;
 
     private AudioSource audioSource;
     private bool mute = false, stopwatch = false;
@@ -20,7 +22,7 @@ public class AnalogClock : MonoBehaviour
     private float updateTime;
     private int hourRotation = 30;
     private int secondRotation = 6;
-    private System.DateTime startTime;
+    private DateTime currentTime, startTime;
     private Vector3 clockwise = new Vector3(0, 1, 0);
 
     private void Start()
@@ -28,32 +30,34 @@ public class AnalogClock : MonoBehaviour
         audioSource = GetComponent<AudioSource>();
         hourMinuteRotation = hourRotation * twelveHourRotation * 2;
         intervalGUIUpdate = updateInterval / 10;
-        startTime = System.DateTime.Now;
+        startTime = DateTime.Now;
         updateTime = updateInterval;
         InitClock();
     }
 
     private void Update()
     {
+        ControlMute();
+        ControlStopwatch();
         if (Time.time >= updateTime) UpdateClock();
         if (Time.time >= updateGUITime) UpdateGUI();
-
-        ControlStopwatch();
-        ControlMute();
     }
 
     private void ControlMute()
     {
-        if (Input.GetKeyDown(KeyCode.M))
-        {
-            mute = !mute;
-        }
+        if (Input.GetKeyDown(KeyCode.M)) mute = !mute;
         if (mute) bottomLeft.text = "M = restore audio"; else bottomLeft.text = "M = mute audio";
     }
 
     private void ControlStopwatch()
     {
-        if (stopwatch) stopTime += Time.deltaTime;
+        if (stopwatch)
+        {
+            stopTime += Time.deltaTime;
+            float absoluteAngle = (stopTime % 60) * secondRotation;
+            stopwatchHand.rotation = Quaternion.Euler(0f, absoluteAngle, 0f);
+        }
+
         if (Input.GetKeyDown(KeyCode.Space))
         {
             stopwatch = !stopwatch;
@@ -69,25 +73,32 @@ public class AnalogClock : MonoBehaviour
     {
         hourHand.Rotate(clockwise * startTime.Hour * hourRotation);
         hourHand.Rotate(clockwise * startTime.Minute * hourMinuteRotation);
+
         minuteHand.Rotate(clockwise * startTime.Minute * secondRotation);
         minuteHand.Rotate(clockwise * startTime.Second * minuteRotation);
-        secondHand.Rotate(clockwise * startTime.Second * secondRotation);
-    }
 
-    private void UpdateGUI()
-    {
-        updateGUITime += intervalGUIUpdate;
-        topLeft.text = ("Begun: " + startTime.ToString() + "\nElapsed: " + Time.time.ToString());
-        topRight.text = ((Time.time % 60).ToString() + " :Laptime\n" + stopTime.ToString() + " :Stopwatch");
+        secondHand.Rotate(clockwise * startTime.Second * secondRotation);
     }
 
     private void UpdateClock()
     {
         updateTime += updateInterval;
-        if (!mute) audioSource.PlayOneShot(secondHandFX[Mathf.FloorToInt(Random.Range(0, secondHandFX.Length))]);
+
+        if (!mute) audioSource.PlayOneShot(secondHandFX[Mathf.FloorToInt(URandom.Range(0, secondHandFX.Length))]);
+
         secondHand.Rotate(clockwise * secondRotation);
         minuteHand.Rotate(clockwise * minuteRotation);
         hourHand.Rotate(clockwise * twelveHourRotation);
-        if (stopwatch) stopwatchHand.Rotate(clockwise * secondRotation);
+
+        currentTime = DateTime.Now;
+        bottomRight.text = currentTime.ToLongTimeString();
+    }
+
+    private void UpdateGUI()
+    {
+        updateGUITime += intervalGUIUpdate;
+
+        topLeft.text = ("Begun: " + startTime.ToString() + "\nElapsed: " + Time.time.ToString());
+        topRight.text = (stopTime.ToString() + " :Stopwatch (space)\n" + (Time.time % 60).ToString() + " :Laptime");
     }
 }
